5bbfdcd19a6fe487fa263513bef1f137bd1656bf
Cameron Russell <2024russellcameronm@aaps.k12.mi.us>
2020-12-08 19:58:10 -0500

    Added sum and average

--- min_max.py
+++ min_max.py
@@ -7,11 +7,17 @@ def min_max():
                 lst.sort()
                 min = lst[0]
                 max = lst[-1]
+                total = sum(lst)
+                ave = total / len(lst)
             except:
                 min = None
                 max = None
+                total = None
+                ave = None
             print("Maximum is", max)
             print("Minimum is", min)
+            print("Sum is", total)
+            print("Average is", ave)
             break
         try:
             num = int(num)
